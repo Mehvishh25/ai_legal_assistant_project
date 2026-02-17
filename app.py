e5c1f32214d5468f251826cea1bddd55822308f6
Mehvish Waheed <mehvish.waheed.25@gmail.com>
2026-02-17 19:10:50 +0530

    test: comments removed

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-# app.py
 from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 import os
 
-# Blueprints
 from summary import summary_bp
 from chatbot import chatbot_bp
 from pdf_qa import pdfqa_bp
@@ -11,13 +9,12 @@ from legal_drafter import drafter_bp
 from case_finder import case_finder_bp
 from adversarial_argument_generator import adversary_bp
 from news import news_bp
-from bot_webhook import bot_bp  # Webhook-based Telegram bot
+from bot_webhook import bot_bp
 
 load_dotenv()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-# Register blueprints
 app.register_blueprint(summary_bp)
 app.register_blueprint(chatbot_bp)
 app.register_blueprint(pdfqa_bp)
